04d959399f60e522bbb852098df3f725061eb02b
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-17 12:32:32 +0200

    creacion

---
+++ src/main/java/org/svalero/util/Constants.java
@@ -0,0 +1,15 @@
+package org.svalero.util;
+
+public class Constants {
+    public static final String DATABASE = "actividadessanvalero";
+    public static final String USERNAME = "JMARTINEZ";
+    public static final String PASSWORD = "JMARTINEZ";
+
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/" + DATABASE ;
+
+    public static final String PARSE_DATE_PATTERN = "yyyy-MM-dd";
+
+    public static final String FORMAT_DATE_PATTERN = "dd/MM/yyyy";
+
+}
