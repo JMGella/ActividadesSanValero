a084785934126e5a46e71b5a7a70183efa8d1822
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-17 13:46:58 +0200

    mapper user

--- src/main/java/org/svalero/dao/UserMapper.java
+++ src/main/java/org/svalero/dao/UserMapper.java
@@ -1,4 +1,19 @@
 package org.svalero.dao;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.svalero.domain.User;
 
-public class UserMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class UserMapper implements RowMapper<User> {
+    @Override
+    public User map(ResultSet rs, StatementContext ctx) throws SQLException {
+        return new User(rs.getInt("user_id"),
+                rs.getString("user_name"),
+                rs.getString("user_lastname"),
+                rs.getInt("user_phone"));
+
+
+    }
+
 }
