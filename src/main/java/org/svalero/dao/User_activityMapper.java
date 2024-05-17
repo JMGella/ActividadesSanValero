b9ad07156363ccfe59cbec38a08f790056ed4902
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-17 16:42:38 +0200

    mapper useractivity

--- src/main/java/org/svalero/dao/User_activityMapper.java
+++ src/main/java/org/svalero/dao/User_activityMapper.java
@@ -1,4 +1,16 @@
 package org.svalero.dao;
 
-public class User_activityMapper {
+import org.jdbi.v3.core.statement.StatementContext;
+import org.svalero.domain.User_activity;
+import org.jdbi.v3.core.mapper.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class User_activityMapper implements RowMapper<User_activity> {
+    @Override
+    public User_activity map(ResultSet rs, StatementContext ctx) throws SQLException {
+        return new User_activity(rs.getInt("reg_id"),
+                rs.getInt("user_id"),
+                rs.getInt("act_id"));
+    }
 }
