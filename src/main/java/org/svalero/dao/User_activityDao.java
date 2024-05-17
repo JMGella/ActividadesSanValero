26229c09dd2de0501dec85332ddf65674bceb488
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-17 17:54:06 +0200

    useractivity dao

--- src/main/java/org/svalero/dao/User_activityDao.java
+++ src/main/java/org/svalero/dao/User_activityDao.java
@@ -1,4 +1,15 @@
 package org.svalero.dao;
 
-public class User_activityDao {
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.svalero.domain.User_activity;
+
+import java.util.List;
+
+public interface User_activityDao {
+    @SqlQuery("SELECT * FROM users_activities")
+    List<User_activity> getAllregister();
+
+    @SqlUpdate("INSERT INTO users_activities (user_id, act_id) VALUES (? , ?)")
+    int addRregister(int user_id, int act_id);
 }
