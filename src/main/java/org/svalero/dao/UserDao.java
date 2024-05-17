5133ece2236e1ab1a333c342fe9929ea183cf17d
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-05-17 17:49:59 +0200

    user dao

--- src/main/java/org/svalero/dao/UserDao.java
+++ src/main/java/org/svalero/dao/UserDao.java
@@ -1,5 +1,14 @@
 package org.svalero.dao;
 
-public class UserDao {
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.svalero.domain.User;
+import java.util.List;
 
+public interface UserDao {
+    @SqlQuery("SELECT * FROM users")
+    List<User> getAllUsers();
+
+    @SqlUpdate("INSERT INTO users (user_name, user_lastname, user_phone) VALUES ( ? , ? , ?)")
+    int addUser(String user_name, String user_lastname, String user_phone);
 }
